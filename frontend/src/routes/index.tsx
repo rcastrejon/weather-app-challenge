4d8458038d550f396e2c974c90fa80a3da52a8d9
Rodrigo Castrejon <me@rcastrejon.dev>
2025-01-26 18:21:04 -0600

    set up tremor.so components

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button"
 import { createFileRoute } from "@tanstack/react-router"
 
 export const Route = createFileRoute("/")({
@@ -8,6 +9,7 @@ function HomeComponent() {
   return (
     <div className="p-2">
       <h3>Hello, world!</h3>
+      <Button>Noop</Button>
     </div>
   )
 }
